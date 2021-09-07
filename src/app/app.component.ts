e1d988c6e409afbaf983de02f123fe65f68ffc2e
WhenixD <whenix.developer@gmail.com>
2021-09-07 11:42:57 -0500

    Angular basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
 import { Component } from '@angular/core';
+import { Product } from './product.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'store-angular';
+  items = ['angular', 'react', 'vue', 'svelte'];
+  products: Product[] = [
+    {
+      id: '1',
+      image: '../assets/img/camiseta.png',
+      title: 'Camiseta',
+      price: 80000,
+      description: 'Lorem ipsum dolor sit amet.',
+    },
+    {
+      id: '2',
+      image: '../assets/img/hoodie.png',
+      title: 'Hoodie',
+      price: 80000,
+      description: 'Lorem ipsum dolor sit amet.',
+    },
+    {
+      id: '3',
+      image: '../assets/img/mug.png',
+      title: 'Mug',
+      price: 80000,
+      description: 'Lorem ipsum dolor sit amet.',
+    },
+    {
+      id: '4',
+      image: '../assets/img/pin.png',
+      title: 'Pin',
+      price: 80000,
+      description: 'Lorem ipsum dolor sit amet.',
+    },
+    {
+      id: '5',
+      image: '../assets/img/stickers1.png',
+      title: 'Stickers',
+      price: 80000,
+      description: 'Lorem ipsum dolor sit amet.',
+    },
+    {
+      id: '6',
+      image: '../assets/img/stickers2.png',
+      title: 'Stickers',
+      price: 80000,
+      description: 'Lorem ipsum dolor sit amet.',
+    },
+  ];
+  addItem() {
+    this.items.push('new item');
+  }
+  deleteItem(index: number) {
+    this.items.splice(index, 1);
+  }
 }
