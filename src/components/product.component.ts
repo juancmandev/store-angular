fb5742eba7a675cdb4854fb60ca890b2b46992b7
WhenixD <whenix.developer@gmail.com>
2021-09-21 20:22:48 -0500

    grid for the products

--- src/components/product.component.ts
+++ src/components/product.component.ts
@@ -12,6 +12,7 @@ import { Product } from '../app/product.model';
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss',]
 })
 export class ProductComponent implements OnInit, DoCheck, OnDestroy {
   @Input() product: Product;
